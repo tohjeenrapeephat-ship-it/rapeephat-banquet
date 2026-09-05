16c0dd13ae3c929ceda9a77ac012f5f4e452e5e0
tohjeenrapeephat <tohjeen.rapeephat@gmail.com>
2026-09-05 21:10:40 +0700

    feat: force update cache-busted gaengpa hotpot image to 2026 version

--- frontend/src/utils/dishImageHelper.ts
+++ frontend/src/utils/dishImageHelper.ts
@@ -104,7 +104,7 @@ export const getDishImage = (dishName: string = '', courseTitle: string = ''): s
     return '/images/dishes/hotpots/hotpot-gaengsom-cha-om-kung.jpg';
   }
   if (['แกงป่า', 'โฮกฮือ', 'ต้มโคล้ง'].some((k) => d.includes(k))) {
-    return '/images/dishes/hotpots/hotpot-gaengpa-ruammit.jpg';
+    return '/images/dishes/hotpots/hotpot-gaengpa-ruammit-2026.jpg?v=20260905';
   }
 
   // --- 5. Rice & Noodles (ข้าวผัด & ผัดหมี่มงคล - ตรวจสอบความถูกต้องของทุกเมนูข้าวผัด) ---
